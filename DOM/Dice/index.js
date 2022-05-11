487d47195ce2b34bce0c6a3aa2f440528d984607
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-11 13:15:25 -0500

    successfully generates different dice images

--- DOM/Dice/index.js
+++ DOM/Dice/index.js
@@ -4,3 +4,5 @@ var randomDiceImage1 = "./images/dice" + randomNumber1 + ".png";
 var randomDiceImage2 = "./images/dice" + randomNumber2 + ".png";
 var image1 = document.querySelectorAll("img")[0];
 var image2 = document.querySelectorAll("img")[1];
+image1.setAttribute("src", randomDiceImage1);
+image2.setAttribute("src", randomDiceImage2);
