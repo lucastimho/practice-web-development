272f88631bf38ebc73ec9f1a80b984533711f15b
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-11 13:12:59 -0500

    uses DOM methods to manipulate img tag

--- DOM/Dice/index.js
+++ DOM/Dice/index.js
@@ -2,3 +2,5 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage1 = "./images/dice" + randomNumber1 + ".png";
 var randomDiceImage2 = "./images/dice" + randomNumber2 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
