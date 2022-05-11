e270950163ccbe355b28681b0e387af728966e3c
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-11 13:18:55 -0500

    displays message based off dice results

--- DOM/Dice/index.js
+++ DOM/Dice/index.js
@@ -6,3 +6,10 @@ var image1 = document.querySelectorAll("img")[0];
 var image2 = document.querySelectorAll("img")[1];
 image1.setAttribute("src", randomDiceImage1);
 image2.setAttribute("src", randomDiceImage2);
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
