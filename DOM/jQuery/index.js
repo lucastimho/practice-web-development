7d33be2abb8d62a40e8b81891005e97e6bbbff11
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-23 18:30:02 -0500

    adds method for clicking h1 to turn purple

--- DOM/jQuery/index.js
+++ DOM/jQuery/index.js
@@ -3,3 +3,6 @@ $("h1").removeClass("big-title");
 $("h1").text("Bye");
 // $("button").html("<em>Hey</em>");
 $("a").attr("href", "https://www.yahoo.com");
+$("h1").click(function () {
+  $("h1").css("color", "purple");
+});
