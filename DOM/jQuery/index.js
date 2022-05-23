1914c505dbbe7f81132185cbbdd21184aafeb168
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-23 18:26:49 -0500

    changes href using js

--- DOM/jQuery/index.js
+++ DOM/jQuery/index.js
@@ -2,4 +2,4 @@ $("h1").addClass("big-title");
 $("h1").removeClass("big-title");
 $("h1").text("Bye");
 // $("button").html("<em>Hey</em>");
-$("button").text("<em>Hey</em>");
+$("a").attr("href", "https://www.yahoo.com");
