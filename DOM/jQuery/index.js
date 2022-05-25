faeff6fabea9581c4427fb4ebb2c2e9528285f9d
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-24 19:50:01 -0500

    changes key presses to display as the h1

--- DOM/jQuery/index.js
+++ DOM/jQuery/index.js
@@ -9,6 +9,6 @@
 $("button").click(function () {
   $("h1").css("color", "purple")
 });
-$("input").keypress(function(event) {
-  console.log(event.key);
+$(document).keypress(function(event) {
+  $("h1").text(event.key);
 });
