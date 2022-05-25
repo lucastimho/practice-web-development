07846663608e038e39e684b18c383c2afe58775a
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-24 19:11:59 -0500

    adds jQuery javascript to allow buttons to turn 'hello' purple

--- DOM/jQuery/index.js
+++ DOM/jQuery/index.js
@@ -1,8 +1,11 @@
-$("h1").addClass("big-title");
-$("h1").removeClass("big-title");
-$("h1").text("Bye");
+// $("h1").addClass("big-title");
+// $("h1").removeClass("big-title");
+// $("h1").text("Bye");
 // $("button").html("<em>Hey</em>");
-$("a").attr("href", "https://www.yahoo.com");
-$("h1").click(function () {
-  $("h1").css("color", "purple");
-});
+// $("a").attr("href", "https://www.yahoo.com");
+// $("h1").click(function () {
+//   $("h1").css("color", "purple");
+// });
+$("button").click(function () {
+  $("h1").css("color", "purple")
+})
