71208293cc47cb21e7b3d1462a3b8b8a6f9041fa
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-24 19:54:46 -0500

    adds jQuery methods to add elements

--- DOM/jQuery/index.js
+++ DOM/jQuery/index.js
@@ -11,4 +11,8 @@ $("button").click(function () {
 });
 $("h1").on("mouseover", function() {
   $("h1").css("color", "purple");
-})
+});
+$("h1").before("<button>New</button>")
+$("h1").after("<button>New</button>")
+$("h1").prepend("<button>New</button>")
+$("h1").append("<button>New</button>")
