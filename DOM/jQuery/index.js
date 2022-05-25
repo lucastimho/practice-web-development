fc8bd91c6c3f3e0f45f2312a9f99e559e4fa670b
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-24 19:51:48 -0500

    changes mouseover to turn h1 to purple

--- DOM/jQuery/index.js
+++ DOM/jQuery/index.js
@@ -9,6 +9,6 @@
 $("button").click(function () {
   $("h1").css("color", "purple")
 });
-$(document).keypress(function(event) {
-  $("h1").text(event.key);
-});
+$("h1").on("mouseover", function() {
+  $("h1").css("color", "purple");
+})
