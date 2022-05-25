1bce98d5ea8355ddf856181a1a04b0c7f1232eb2
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-24 19:34:40 -0500

    adds css styling to h1

--- DOM/jQuery/index.js
+++ DOM/jQuery/index.js
@@ -1,1 +1,11 @@
-$("h1").css("color", "red");
+// $("h1").addClass("big-title");
+// $("h1").removeClass("big-title");
+// $("h1").text("Bye");
+// $("button").html("<em>Hey</em>");
+// $("a").attr("href", "https://www.yahoo.com");
+// $("h1").click(function () {
+//   $("h1").css("color", "purple");
+// });
+$("button").click(function () {
+  $("h1").css("color", "purple")
+})
