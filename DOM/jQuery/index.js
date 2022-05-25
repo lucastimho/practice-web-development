30739afcb977e7197a4800e186add2b45cc010dd
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-24 20:01:50 -0500

    adds fade toggle to button

--- DOM/jQuery/index.js
+++ DOM/jQuery/index.js
@@ -12,7 +12,10 @@ $("button").click(function () {
 $("h1").on("mouseover", function() {
   $("h1").css("color", "purple");
 });
-$("h1").before("<button>New</button>")
-$("h1").after("<button>New</button>")
-$("h1").prepend("<button>New</button>")
-$("h1").append("<button>New</button>")
+// $("h1").before("<button>New</button>")
+// $("h1").after("<button>New</button>")
+// $("h1").prepend("<button>New</button>")
+// $("h1").append("<button>New</button>")
+$("button").on("click", function() {
+  $("h1").fadeToggle()
+})
